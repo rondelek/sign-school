ed1d91cb5dd09ed71626da5d4ef318c48987e8d7
rondelek <ewelina.nowak103@gmail.com>
2022-09-01 15:19:01 +0200

    added media

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -12,8 +12,8 @@ function App() {
     <div className="App">
       <Header/>
       <Hero/>
-      <Kids/>
       <Adults/>
+      <Kids/>
       <LessonFirst/>
       <Why/>
     </div>
--- src/components/Kids/Kids.jsx
+++ src/components/Kids/Kids.jsx
@@ -7,6 +7,9 @@ import './Kids.css';
 function Kids() {
     return (
         <div className="kids">
+            <div className="kids-img">
+                <img src={kids} alt="" />
+            </div>
             <div className="kids-text">
                 <h2 className="kids-text-h2">
                     Kursy dla dzieci
@@ -15,9 +18,6 @@ function Kids() {
                     Chcesz, aby Twoje dziecko podniosło swoje kompetencje z języka angielskiego, śmiało weszło w dorosły świat i było przygotowane do wejścia na rynek pracy? A może Twoja pociecha nie przepada za nauką angielskiego w szkole i mało z niej wynosi? Ten kurs rozwiąże wszelkie problemy! Kursy językowe dla dzieci są przygotowywane w taki sposób, aby w sposób efektywny oraz interesujący dla młodego odbiorcy pomóc mu w komunikacji w języku angielskim.
                 </p>
                 <More/>
-            </div>
-            <div className="kids-img">
-                <img src={kids} alt="" />
             </div>
         </div>
     )
